e3257349bcbfe205b458be9e294068f5529ba39a
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2025-01-14 09:56:14 +0100

    minor fixes

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -81,7 +81,7 @@ def main():
         kwargs['slurm_comment'] = args.comment
 
     executor.update_parameters(
-        mem_gb=40 * num_gpus_per_node,
+        mem_gb=60 * num_gpus_per_node,
         gpus_per_node=num_gpus_per_node, #num_gpus_per_node
         tasks_per_node=num_gpus_per_node,  # one task per GPU num_gpus_per_node
         cpus_per_task=1,
